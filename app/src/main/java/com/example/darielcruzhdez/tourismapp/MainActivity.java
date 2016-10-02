969b5e3c236b1a96549407a56228b4fd6f84e743
Dariel Cruz Hernandez <darielnaturalist@gmail.com>
2016-10-02 00:32:28 -0400

    Adding views to the mainactivity

--- app/src/main/java/com/example/darielcruzhdez/tourismapp/MainActivity.java
+++ app/src/main/java/com/example/darielcruzhdez/tourismapp/MainActivity.java
@@ -8,8 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Spinner mSpinnerCity, mSpinnerDestination;
+    private Button mButtonShowMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
