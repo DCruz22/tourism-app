1d0cc9156ded77b0b2dfde8337b20d5504c59357
Dariel Cruz Hernandez <darielnaturalist@gmail.com>
2016-10-09 20:11:29 -0400

    Adding functionalities to the main activity

--- app/src/main/java/com/example/darielcruzhdez/tourismapp/main/activities/MainActivity.java
+++ app/src/main/java/com/example/darielcruzhdez/tourismapp/main/activities/MainActivity.java
@@ -1,23 +1,44 @@
 package com.example.darielcruzhdez.tourismapp.main.activities;
 
+import com.example.darielcruzhdez.tourismapp.main.adapters.SpinAdapter;
+import com.example.darielcruzhdez.tourismapp.main.models.City;
+import com.google.gson.Gson;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.darielcruzhdez.tourismapp.R;
 
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = "MainActivity";
 
     private Spinner mSpinnerCity, mSpinnerDestination;
     private Button mButtonShowMap;
+
+    private static List<City> mCities;
+
+    private SpinAdapter mCitiesAdapter;
+    private ArrayAdapter<String> mDestinationsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +47,60 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Gson gson = new Gson();
+
+        try {
+            Resources res = getResources();
+            InputStream in_s = res.openRawResource(R.raw.locations);
+
+            byte[] b = new byte[in_s.available()];
+            in_s.read(b);
+
+            mCities = Arrays.asList(gson.fromJson(new String(b), City[].class));
+
+            mCitiesAdapter = new SpinAdapter(this, R.layout.content_main, mCities);
+
+            mButtonShowMap = (Button) findViewById(R.id.btnShowInMap);
+            mSpinnerCity = (Spinner) findViewById(R.id.citySpinner);
+            mSpinnerDestination = (Spinner) findViewById(R.id.destinationsSpinner);
+
+            mSpinnerCity.setPromptId(R.string.select_city);
+            mSpinnerDestination.setPromptId(R.string.select_destination);
+
+            mSpinnerCity.setAdapter(mCitiesAdapter);
+        } catch (Exception e) {
+            // e.printStackTrace();
+            Log.i(TAG, e.getLocalizedMessage());
+        }
+
+        mSpinnerCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, position, Toast.LENGTH_SHORT).show();
+
+                if(position > 0){
+                    String[] destinations = mCitiesAdapter.getItem(position).getDestinations();
+                    mDestinationsAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.content_main, destinations);
+                    mDestinationsAdapter.setDropDownViewResource(R.layout.content_main);
+                    mSpinnerDestination.setAdapter(mDestinationsAdapter);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
-
-        mButtonShowMap = (Button) findViewById(R.id.btnShowInMap);
-        mSpinnerCity = (Spinner) findViewById(R.id.citySpinner);
-        mSpinnerDestination = (Spinner) findViewById(R.id.destinationsSpinner);
 
         mButtonShowMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=1600 Amphitheatre Parkway, Mountain+View, California");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
 
             }
         });
@@ -69,4 +127,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
